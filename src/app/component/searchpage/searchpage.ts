7e2b003edf07df9c655d8fda61873e92c7d4990b
mohamed mahmoud eid <mm4574@fayoum.edu.eg>
2025-09-05 16:01:46 +0300

    setup our project component

---
+++ src/app/component/searchpage/searchpage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchpage',
+  imports: [],
+  templateUrl: './searchpage.html',
+  styleUrl: './searchpage.css'
+})
+export class Searchpage {
+
+}
