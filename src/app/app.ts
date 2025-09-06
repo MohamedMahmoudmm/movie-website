8e554be214fe15ea5a04da0084d3825188c9f97e
Montaser Ismail <montaserismail20@gmail.com>
2025-09-06 19:04:57 +0300

    Add edit Profile component

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,12 +5,12 @@ import { Register } from './component/register/register';
 import { Details } from './component/details/details';
 import { Header } from './component/header/header';
 import { AccountDetails } from './component/account-details/account-details';
+import { EditProfile } from './component/edit-profile/edit-profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Details, AccountDetails],
+  imports: [RouterOutlet, Details, AccountDetails, EditProfile],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-}
+export class App {}
