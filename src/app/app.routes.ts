77c0c9b96f1809a40b16cead1de457482d43b732
Montaser Ismail <montaserismail20@gmail.com>
2025-09-06 19:04:57 +0300

    Add edit Profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Mainpage } from './component/mainpage/mainpage';
 import { Searchpage } from './component/searchpage/searchpage';
 import { Details } from './component/details/details';
 import { AccountDetails } from './component/account-details/account-details';
+import { EditProfile } from './component/edit-profile/edit-profile';
 
 export const routes: Routes = [
   { path: '', component: Mainpage, title: 'Movie App' },
   { path: 'search', component: Searchpage, title: 'Search' },
   { path: 'details', component: Details, title: 'Details' },
   { path: 'account-details', component: AccountDetails, title: 'AccountDetails' },
+  { path: 'edit-profile', component: EditProfile, title: 'EditProfile' },
 
   { path: '**', redirectTo: '' },
 ];
