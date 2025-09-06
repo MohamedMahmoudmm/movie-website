5991d6203b0864517d06820d0522262bb9784a6c
mohamed mahmoud eid <mm4574@fayoum.edu.eg>
2025-09-06 17:28:35 +0300

    some enhancement

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { Mainpage } from './component/mainpage/mainpage';
 import { Searchpage } from './component/searchpage/searchpage';
 import { Details } from './component/details/details';
+import { Login } from './component/login/login';
+import { Register } from './component/register/register';
 
 export const routes: Routes = [
     { path: '', component: Mainpage, title: 'Movie App' },
   { path: 'search', component: Searchpage, title: 'Search' },
+  { path: 'login', component: Login, title: 'login' },
+  { path: 'register', component: Register, title: 'register' },
   {path : 'details' , component:Details,title:'Details'},
 
   
--- src/app/component/card-components/card-components.ts
+++ src/app/component/card-components/card-components.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MovieModel } from '../../models/movie-model';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-card-components',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './card-components.html',
   styleUrl: './card-components.css'
 })
