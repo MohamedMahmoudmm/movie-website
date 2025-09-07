225a74b61420da20d05d0b19481f93e1712eb770
mohamed mahmoud eid <mm4574@fayoum.edu.eg>
2025-09-07 23:34:39 +0300

    add http and auth logic

--- src/app/component/login/login.ts
+++ src/app/component/login/login.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { HttpService } from '../../services/http-service';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,24 @@ import { RouterLink } from "@angular/router";
   styleUrl: './login.css'
 })
 export class Login {
+  Msg: string = ''
+
+  constructor(private http: HttpService) {
+    http.login('Mohamed_2001', 'Mohamed_2001').subscribe({
+      next: (session) => {
+        localStorage.setItem('session_id', session.session_id);
+        this.Msg = 'Login Success'
+      },
+      error: (err) => {
+        this.Msg = err.message
+      }
+    })
+
+    // http.get('movie/now_playing').subscribe({
+      
+    //     console.log(movies)
+      
+    // })
+  }
 
 }
