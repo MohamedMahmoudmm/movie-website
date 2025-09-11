79ae48b83482dfde001bf9313105a83795a977f2
mohamed mahmoud eid <mm4574@fayoum.edu.eg>
2025-09-11 08:33:37 +0300

    wishlist

--- src/app/component/wishlist/wishlist.ts
+++ src/app/component/wishlist/wishlist.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
+import { Header } from '../header/header';
+import { HttpService } from '../../services/http-service';
+import { MovieModel } from '../../models/movie-model';
 
 @Component({
   selector: 'app-wishlist',
-  imports: [],
+  imports: [Header],
   templateUrl: './wishlist.html',
   styleUrl: './wishlist.css'
 })
 export class Wishlist {
+  favList:MovieModel[]=[]
+  sessionId:string=''
+  constructor(private http: HttpService) {}
+
+  ngOnInit() {
+    this.sessionId = localStorage.getItem('session_id')??'';
+    this.getFav()
+  }
+  getFav(){
+    this.favList=[]
+    this.http.get(`account/21908959/favorite/movies?session_id=${this.sessionId}`).subscribe({
+      next: (movies) => {
+        console.log(movies.results);
+        movies.results.forEach((element: MovieModel) => {
+          element.inFav = true
+        });
+        this.favList = movies.results
+      }
+    })
+  }
+
+  toggleFavorite(movie: MovieModel, event: Event) {
+  event.stopPropagation(); 
+  const body = {
+    media_type: 'movie',
+    media_id: movie.id,
+    favorite: !movie.inFav
+  }
+
+  this.http.post(body, `account/21908959/favorite?session_id=${this.sessionId}`)
+    .subscribe({
+      next: () => {
+        movie.inFav = !movie.inFav;
+        this.getFav()
+      },
+      error: (err) => console.error('Error adding to favorite:', err)
+    });
+    console.log(movie.inFav);
+    
+}
 
 }
--- src/app/services/common-services.ts
+++ src/app/services/common-services.ts
@@ -1,37 +1,23 @@
-import { Component } from '@angular/core';
-import { Header } from '../header/header';
-import { Search } from '../search/search';
-import { CardComponents } from '../card-components/card-components';
-import { MovieModel } from '../../models/movie-model';
-import { HttpService } from '../../services/http-service';
-import { Footer } from '../footer/footer';
+import { Injectable } from '@angular/core';
+import { MovieModel } from '../models/movie-model';
+import { HttpService } from './http-service';
 
-@Component({
-  selector: 'app-mainpage',
-  imports: [Header, Search, CardComponents, Footer],
-  templateUrl: './mainpage.html',
-  styleUrl: './mainpage.css',
+@Injectable({
+  providedIn: 'root'
 })
-export class Mainpage {
-  movies: MovieModel[] = [];
+export class CommonServices {
+  
+movies: MovieModel[] = [];
   favList:MovieModel[]=[];
   watchList:MovieModel[]=[];
   
-  // Pagination state
-  totalPages: number = 1;
-  currentPage: number = 1;
 
   sessionId : string ='';
-  constructor(private http : HttpService){
-  }
-
-   ngOnInit() {
+  constructor(private http:HttpService){
     this.sessionId = localStorage.getItem('session_id')??'';
 
-    this.getAllMovie();
-    this.getFav();
-    this.getWatchList();
   }
+
  getFav(){
     this.http.get(`account/21908959/favorite/movies?session_id=${this.sessionId}`).subscribe({
       next: (movies) => {
@@ -40,6 +26,7 @@ export class Mainpage {
         this.updateFavOnMainPage()
       }
     })
+    return this.favList
   }
  getWatchList(){
     this.http.get(`account/21908959/watchlist/movies?session_id=${this.sessionId}`).subscribe({
@@ -49,30 +36,29 @@ export class Mainpage {
         this.updateWatchOnMainPage()
       }
     })
+    return this.watchList
   }
- 
-  getAllMovie(page: number = 1) {
-    this.http.get(`movie/popular?page=${page}`).subscribe({
+  getAllMovie()
+  {
+    this.http.get('movie/now_playing').subscribe({
       next: (movies) => {
         console.log(movies.results);
         
         this.movies = movies.results;
-        this.totalPages = Math.min(movies.total_pages,500);
-        this.currentPage = movies.page;
         this.updateFavOnMainPage()
         this.updateWatchOnMainPage()
+        
       }
     })
-  }
-
-  onPageChange(page: number) {
-    this.getAllMovie(page);
+    return this.movies
   }
 
   updateFavOnMainPage(){
+    
+
   const favIds = new Set(this.favList.map(f => f.id));
   this.movies.forEach(m => {
-    m.inFav = favIds.has(m.id);  // update the flag
+    m.inFav = favIds.has(m.id);  // ✅ update the flag
   });
   }
 
@@ -84,4 +70,5 @@ export class Mainpage {
     m.inWatchlist = favIds.has(m.id);  // ✅ update the flag
   });
   }
+
 }
