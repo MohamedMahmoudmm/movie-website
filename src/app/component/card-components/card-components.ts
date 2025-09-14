1081e48d296c16530418853f7ebfc3ffe52ea9b9
Omar Ali <omarh9392@gmail.com>
2025-09-14 11:02:36 +0300

    finall

--- src/app/component/card-components/card-components.ts
+++ src/app/component/card-components/card-components.ts
@@ -2,8 +2,6 @@ import { Component, inject, Input } from '@angular/core';
 import { MovieModel } from '../../models/movie-model';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { HttpService } from '../../services/http-service';
-import { AccountService } from '../../services/account-service';
 import { CommonServices } from '../../services/common-services';
 
 @Component({
